257b43879e19011835bdfb9fa7d9380b2afbddb8
Aidan Steele <aidan.steele@glassechidna.com.au>
2023-05-04 11:30:06 +1000

    add (basic) support for Image-based lambda functions

--- macro/macro.py
+++ macro/macro.py
@@ -28,20 +28,29 @@ def handle(name, resource):
     env = props['Environment'].get('Variables', {})
     props['Environment']['Variables'] = env
 
+    package_type = props.get('PackageType', 'Zip')
+
     if len(secrets) > 0:
-        layers = props.get('Layers', [])
-        props['Layers'] = layers
+        if package_type == 'Zip':
+            layers = props.get('Layers', [])
+            props['Layers'] = layers
 
-        if props.get('Architectures', ['x86_64'])[0] == 'arm64':
-            layers.append(os.environ.get('LayerArm64'))
-        else:
-            layers.append(os.environ.get('LayerX8664'))
+            if props.get('Architectures', ['x86_64'])[0] == 'arm64':
+                layers.append(os.environ.get('LayerArm64'))
+            else:
+                layers.append(os.environ.get('LayerX8664'))
 
-        env['AWS_LAMBDA_EXEC_WRAPPER'] = '/opt/cloudenv'
+            env['AWS_LAMBDA_EXEC_WRAPPER'] = '/opt/cloudenv'
 
-        runtime = props['Runtime']
-        if runtime == 'provided' or runtime == 'provided.al2':
-            layers.append(os.environ.get('BootstrapLayer'))
+            runtime = props['Runtime']
+            if runtime == 'provided' or runtime == 'provided.al2':
+                layers.append(os.environ.get('BootstrapLayer'))
+        elif package_type == 'Image':
+            image_config = props.get('ImageConfig', {})
+            entrypoint = image_config.get('EntryPoint', [])
+            entrypoint = ['/opt/cloudenv'].extend(entrypoint)
+            image_config['EntryPoint'] = entrypoint
+            props['ImageConfig'] = image_config
 
     ssmParams = []
     smSecrets = []
